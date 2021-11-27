b8b56ebfe7f8504ab8c6f54c686cfe8b4f4ddc1e
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-11-27 20:21:34 +0300

    section 1 completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>React with Redux application!</h1>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
